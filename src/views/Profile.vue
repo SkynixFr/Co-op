0c10249925f001df7d09ac9d80cc4869ba1ccdaf
Skynix <romain.day.ludovic@gmail.com>
2020-02-19 14:20:01 +0100

    update(profile): responsive, style
    user can see her profile on desktop, mobile and tablet

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,8 +1,36 @@
 <template>
-  <div>
-    <h1>Profil</h1>
-    {{ membre.fullname }}
-  </div>
+  <v-container fluid>
+    <v-row justify="center" no-gutters>
+      <v-col cols="12" md="4" sm="6">
+        <v-card outlined>
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="overline mb-4">Profile</div>
+              <v-list-item-title class="headline mb-1">
+                {{ member.fullname }}
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ member.email }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-avatar
+              tile
+              size="100"
+              color="grey"
+            ></v-list-item-avatar>
+          </v-list-item>
+
+          <v-card-actions>
+            <v-btn icon color="primary"><v-icon>mdi-twitter</v-icon></v-btn>
+            <v-btn icon color="indigo darken-2"
+              ><v-icon>mdi-facebook-messenger</v-icon></v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -10,7 +38,7 @@ export default {
   name: "Profil",
   data() {
     return {
-      membre: this.getMember(this.$route.params.id)
+      member: this.getMember(this.$route.params.id)
     };
   },
   mounted() {
